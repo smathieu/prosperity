59c303e612de28bfc647a5b02b28ca275b4b2cb5
Simon Mathieu <simon.math@gmail.com>
2013-11-01 18:18:07 -0700

    Count extractor implementation

--- lib/prosperity/extractors/count.rb
+++ lib/prosperity/extractors/count.rb
@@ -1,5 +1,14 @@
 module Prosperity
   class Extractors::Count < Extractors::Base
+    def to_a
+      data = []
+
+      period.each_period(start_time, end_time) do |start_time|
+        data << @metric.scope.where("#{metric.group_by} < ?", start_time).count
+      end
+
+      data
+    end
   end
 end
 
--- spec/lib/prosperity/extractors/count_spec.rb
+++ spec/lib/prosperity/extractors/count_spec.rb
@@ -2,6 +2,34 @@ require 'spec_helper'
 
 module Prosperity
   describe Extractors::Count do
+    let(:start_time) { 1.year.ago }
+    let(:end_time) { start_time + 1.year }
+    let(:period) { Periods::MONTH }
 
+    subject { Extractors::Count.new(metric, start_time, end_time, period) }
+
+    before do 
+      User.delete_all
+      [2.years.ago, 1.month.ago, 1.month.from_now].each do |time|
+        User.create created_at: time
+      end
+    end
+
+    context "simple scope" do
+      let(:metric) { OpenStruct.new(scope: User, group_by: :created_at) }
+
+      describe "#to_a" do
+        let(:data) { subject.to_a }
+
+        it "returns the one entry per period" do
+          data.size.should == 12  
+        end
+
+        it "returns the counts at it increases" do
+          data[0].should == 1
+          data[-1].should == 2
+        end
+      end
+    end
   end
 end
