8c156fa1e5b0a69c903647092073ca88d8dc6932
Russell Smith <russell.smith@ukd1.co.uk>
2013-11-26 20:42:22 -0800

    Add ceil / floor for inputed dates so that whole periods are displayed as best as possible

--- spec/lib/prosperity/extractors/base_spec.rb
+++ spec/lib/prosperity/extractors/base_spec.rb
@@ -2,7 +2,22 @@ require 'spec_helper'
 
 module Prosperity
   describe Extractors::Base do
+    let(:metric) { UsersMetric.new }
+    subject { Extractors::Change.new(metric, 'default', start_time, end_time, period) }
+    let(:data) { subject.to_a }
 
+    context "Month period" do
+      let(:start_time) { Time.new(1981, 7, 10) }
+      let(:end_time) { Time.new(2012, 7, 10) }
+      let(:period) { Periods::MONTH }
+
+      it "changes the start time to 1981/07/01" do
+        subject.start_time.should == Time.new(1981, 7, 1)
+      end
+
+      it "changes the end time to 2012/07/31" do
+        subject.start_time.should == Time.new(1981, 7, 1)
+      end
+    end
   end
 end
-
